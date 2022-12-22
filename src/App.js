d30b0f1c7755d0ef717f6a8d6412060558634b78
chad hinds <chadrickj8@gmail.com>
2022-12-22 13:47:18 -0500

    Add image search and seperate Card logic

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
 import React, { useState, useEffect } from "react";
-
+import ImageSearch from "./components/ImageSearch";
+import ImageCard from "./components/ImageCard"
 function App() {
   const [images, setImages] = useState([]);
-  const [loading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${term}&image_type=photo&pretty=true`).then(res => res.json())
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
       .then(data => {
         setImages(data.hits);
+        setIsLoading(false);
       }).catch(err => console.log(err))
   }, []);
   console.log(process.env.REACT_APP_API_KEY)
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by someone
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            3000
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-            300
-          </li> <li>
-            <strong>Likes:</strong>
-            3000
-          </li>
-        </ul>
-      </div>
-      <div className="px6 py-4">
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
 
-      </div>
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
   );
 }
