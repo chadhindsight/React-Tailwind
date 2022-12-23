5766ae23dec113c2257a59c7c94f050dec3c2cea
chad hinds <chadrickj8@gmail.com>
2022-12-23 12:26:06 -0500

    Remove comment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ImageCard from "./components/ImageCard"
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // Store search term in react state
   const [term, setTerm] = useState('');
 
   useEffect(() => {
